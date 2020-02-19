348535ea3202a104e719cf075539c2d47e297e57
Adam <adam.lee@students.plymouth.ac.uk>
2020-02-19 15:50:25 +0000

    WIP: re-work account creation form

--- src/app/views/create-account/create-account.page.ts
+++ src/app/views/create-account/create-account.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import AccountAPIService from 'src/app/services/api/account/account-api-service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: "app-create-account",
@@ -9,39 +10,44 @@ import AccountAPIService from 'src/app/services/api/account/account-api-service'
 export class CreateAccountPage implements OnInit {
   
   accountRole: string = "Artist";
-  username: string;
-  email: string;
-  password: string;
-  confirmedPassword: string;
 
-  genres: string[] = [] ;
-  existingGenres: string[] = [
-    "Rock",
-    "Reggea",
-    "Rasta",
-    "Metal",
-    "Punk",
-    "Screamo"
-  ];
+  // genres: string[] = [] ;
+  // existingGenres: string[] = [
+  //   "Rock",
+  //   "Reggea",
+  //   "Rasta",
+  //   "Metal",
+  //   "Punk",
+  //   "Screamo"
+  // ];
 
-  venues: string[] = [];
-  existingVenues: string[] = [
-    "The Cavern - Exeter",
-    "New Quay Inn - Teignmouth",
-    "The Pigs Nose",
-    "Blue Anchor",
-  ];
+  // venues: string[] = [];
+  // existingVenues: string[] = [
+  //   "The Cavern - Exeter",
+  //   "New Quay Inn - Teignmouth",
+  //   "The Pigs Nose",
+  //   "Blue Anchor",
+  // ];
 
-  lat: number = 10.123;
-  lon: number = 100.123;
-  name: string;
-  bio: string;
-  lookingFor: string;
-  matchRadius: number;
+  // lat: number = 10.123;
+  // lon: number = 100.123;
+  // name: string;
+  // bio: string;
+  // lookingFor: string;
+  // matchRadius: number;
   
   loading: boolean = false;
 
-  constructor(private accountAPIService: AccountAPIService) { }
+  constructor(private accountAPIService: AccountAPIService, private formBuilder: FormBuilder) {}
+  
+  createAccountForm = this.formBuilder.group({
+    username: ["", Validators.required], 
+    email: ["", 
+      [Validators.required, Validators.email]
+    ],
+    password: ['', Validators.required],
+    confirmedPassword: ['', Validators.required]
+  })
 
   ngOnInit() {
   }
@@ -51,22 +57,44 @@ export class CreateAccountPage implements OnInit {
     console.log("Role ", this.accountRole);
   }
 
-  async createAccountPressed() {
-    console.log("Entered Values: " 
-    + "\nRole " + this.accountRole
-    + "\nUsername " + this.username
-    + "\nemail " + this.email
-    + "\npassword " + this.password
-    + "\nconfirmed password " + this.confirmedPassword
-    + "\nname " + this.name
-    + "\ngenres " + this.genres
-    + "\nvenues " + this.venues
-    + "\nbio" + this.bio
-    + "\nlooking for" + this.lookingFor
-    + "\nmatch radius " + this.matchRadius);
+  async submit() {
+    console.log(this.createAccountForm.value);
 
-    await this.accountAPIService.createAccont(this.accountRole, this.username, this.email, this.password, this.name,
-      this.lat, this.lon, this.bio, this.lookingFor, this.genres, this.venues, this.matchRadius);
+    //await this.accountAPIService.createAccont(this.accountRole, this.username, this.email, this.password);
+  }
+
+  get username() {
+    return this.createAccountForm.get("username");
+  }
+
+  get email() {
+    return this.createAccountForm.get("email");
+  }
+
+  get password() {
+    return this.createAccountForm.get("password");
+  }
+
+  get confirmedPassword() {
+    return this.createAccountForm.get("confirmedPassword");
+  }
+  
+
+  public errorMessages = {
+    username: [
+      { type: 'required', message: 'Name is required' },
+      { type: 'maxlength', message: 'Name cant be longer than 100 characters' }
+    ],
+    email: [
+      { type: 'required', message: 'Email is required' },
+      { type: 'email', message: 'Please enter a valid email address' }
+    ],
+    password: [
+      { type: 'required', message: 'Password is required' },
+    ],
+    confirmedPassword: [
+      { type: 'required', message: 'Please confirm password' },
+    ],
   }
 
 }
