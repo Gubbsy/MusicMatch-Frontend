6941cdea737b3ec1b1210be6bbed5b859de0e053
Adam Lee <adam.lee@students.plymouth.ac.uk>
2020-02-27 15:50:54 +0000

    WIP: Styling suggestion card

--- src/app/views/suggestion-feed/suggestions-feed.page.ts
+++ src/app/views/suggestion-feed/suggestions-feed.page.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import IAccountDetailsResponse from 'src/app/models/response/account/IAccountDetailsResponse';
 
 @Component({
   selector: "app-sugestions",
@@ -9,7 +10,7 @@ export class SuggestionFeedPage {
 
   private pageTitle = "Suggested Matches";
 
-  cards = [];
+  cards: IAccountDetailsResponse[] = [];
 
   constructor() {
     this.loadSuggestionCards();
@@ -18,26 +19,46 @@ export class SuggestionFeedPage {
   loadSuggestionCards() {
     this.cards = [
       {
-        name: "Boris"
-      },
+        name: "Violet Eye",
+        bio: "We're an up-and-coming alrt-rock/grunge band from Teignmouth. With a pastion for hard hittign riffs and head banging bass, we'll be sure to knock your socks off ",
+        lookingFor: "We're looking to link up with new bands to support us in our ever-growing gig portfolio. As well as new giging opertunities and venues to work with.",
+        genres: ["Alt-rock", "Grunge", "Nu-Metal", "Punk"],
+        venues: ["The nags head", "The Blue Anchor", "Moloys Tavern"],
+        lat: 50.123132,
+        lon: 0.123,
+        matchRadius: 50,
+      }, 
       {
-        name: "Bill"
-      },
+        name: "Boris",
+        bio: "This is ma bio",
+        lookingFor: "I'm looking for friends",
+        genres: ["Pop", "Rock"],
+        venues: ["The nags head"],
+        lat: 50.123132,
+        lon: 0.123,
+        matchRadius: 0.123,
+      }, 
       {
-        name: "Mandy"
-      },
+        name: "Boris",
+        bio: "This is ma bio",
+        lookingFor: "I'm looking for friends",
+        genres: ["Pop", "Rock"],
+        venues: ["The nags head"],
+        lat: 50.123132,
+        lon: 0.123,
+        matchRadius: 0.123,
+      }, 
       {
-        name: "Dehierdra"
-      },
-      {
-        name: "Mr Krabs"
-      },
-      {
-        name: "Aids Boi"
-      },
-      {
-        name: "Lol Cats"
-      },
+        name: "Boris",
+        bio: "This is ma bio",
+        lookingFor: "I'm looking for friends",
+        genres: ["Pop", "Rock"],
+        venues: ["The nags head"],
+        lat: 50.123132,
+        lon: 0.123,
+        matchRadius: 0.123,
+      }, 
+    
     ]
   }
 
