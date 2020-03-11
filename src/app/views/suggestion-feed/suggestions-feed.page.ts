0bc63f2f095ec009f55fa6f8eb385e4d5a7a90cf
Adam Lee <adam.lee@students.plymouth.ac.uk>
2020-03-11 12:44:42 +0000

    Added error handling for getting suggestions

--- src/app/views/suggestion-feed/suggestions-feed.page.ts
+++ src/app/views/suggestion-feed/suggestions-feed.page.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import IAccountDetailsResponse from "src/app/models/response/account/IAccountDetailsResponse";
 import SuggestionsAPIService from "src/app/services/api/suggestions/suggestions-api.service";
+import ErrorToastService from 'src/app/services/error-handling/error-toast.service';
 
 @Component({
   selector: "app-sugestions",
@@ -13,15 +14,25 @@ export class SuggestionFeedPage {
 
   cards: ISuggestionsResponse[];
 
-  constructor(private suggestionsService: SuggestionsAPIService) {
+  constructor(private suggestionsService: SuggestionsAPIService,  private errorToastService: ErrorToastService) {
     this.loadSuggestionCards();
   }
 
   async loadSuggestionCards() {
-    const response = await this.suggestionsService.GetSuggestions();
-    //TODO: Error Checking
+    try {
+      const response = await this.suggestionsService.GetSuggestions();
 
-    this.cards = response.payload;
+      if ((response.errors !== null || response !== undefined) &&  response.errors.length > 0 ) {
+        response.errors.forEach(e => {
+          this.errorToastService.showMultipleToast(e);
+        });
+      
+      this.cards = response.payload;
+      }
+    } catch {
+      this.errorToastService.showMultipleToast("Oops something went wrong");
+    }
+
     console.log(this.cards);
   }
 
