7944c3051ce68acb6c0c0318f810dee4052604eb
Adam Lee <adam.lee@students.plymouth.ac.uk>
2020-03-11 16:28:24 +0000

    Add account-page

---
+++ src/app/views/account-page/account-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-account-page",
+  templateUrl: "./account-page.page.html",
+  styleUrls: ["./account-page.page.scss"],
+})
+export class AccountPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
