020abd62bbb0572ca58f8058b8a572972b6aca7e
Adam Lee <adam.lee@students.plymouth.ac.uk>
2020-03-11 15:42:52 +0000

    Added pop-up for successful match

--- src/app/views/suggestion-feed/suggestions-feed.page.ts
+++ src/app/views/suggestion-feed/suggestions-feed.page.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import IAccountDetailsResponse from "src/app/models/response/account/IAccountDetailsResponse";
 import SuggestionsAPIService from "src/app/services/api/suggestions/suggestions-api.service";
 import ErrorToastService from "src/app/services/error-handling/error-toast.service";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-sugestions",
@@ -14,7 +15,7 @@ export class SuggestionFeedPage {
 
   cards: ISuggestionsResponse[];
 
-  constructor(private suggestionsService: SuggestionsAPIService,  private errorToastService: ErrorToastService) {
+  constructor(private suggestionsService: SuggestionsAPIService,  private errorToastService: ErrorToastService, private toastController: ToastController) {
     this.loadSuggestionCards();
   }
 
@@ -48,9 +49,24 @@ export class SuggestionFeedPage {
       }
       console.log("Did Match: ", response.payload.didMatch);
 
+      if (response.payload.didMatch) {
+        this.displayMatch(event.card.name);
+      }
+
     } catch {
       this.errorToastService.showMultipleToast("Oops something went wrong");
     }
+  }
+
+  async displayMatch(matchName: string) {
+    const matchToast = await this.toastController.create({
+      message: "You've matched with " + matchName + "!",
+      duration: 4000,
+      showCloseButton: true,
+      position: "middle",
+      color: "tertiary"
+    });
+    matchToast.present();
   }
 
 }
